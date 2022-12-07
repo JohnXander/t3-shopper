a36148029eccd48273bbd2499ffd0205785bb07c
John Xander <johnblox5@gmail.com>
2022-12-07 11:49:03 +0100

    set up trpc

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
-import type { AppProps } from 'next/app'
+import type { AppType } from 'next/app';
+import { trpc } from '../utils/trpc';
 import 'tailwindcss/tailwind.css';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp: AppType = ({ Component, pageProps }) => {
+  return <Component {...pageProps} />;
+};
+
+export default trpc.withTRPC(MyApp);
